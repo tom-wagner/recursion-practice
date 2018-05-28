e49c8dc1d8a7947de774ef11c23efd2ceb3e2410
Tom Wagner <wagne868@umn.edu>
2018-05-28 17:11:54 -0400

    Complete 1 - 8 with recursion

--- recursion.js
+++ recursion.js
@@ -7,42 +7,77 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
-
+  while (n > 1) {
+    return n * factorial(n - 1);
+  }
+  return 1;
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
-
+  while (array.length > 1) {
+    return array[0] + sum(array.slice(1));
+  }
+  return array[0];
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
-var arraySum = function(array) {
+var arraySum = function(arr) {
   let total = 0;
 
   const recurse = arr => {
-    
+    for (var i = 0; i < arr.length; i++) {
+      if (Array.isArray(arr[i])) {
+        recurse(arr[i]);
+      } else {
+        total += arr[i];
+      }
+    }
   };
 
-  recurse(array);
+  recurse(arr);
 
   return total;
 };
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+  if (n === 0) {
+    return true;
+  } else if (n === 1) {
+    return false;
+  } else {
+    return isEven(Math.abs(n) - 2);
+  }
 };
 
 // 5. Sum all integers below a given integer.
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
+  while (n > 0) {
+    return (n - 1) + sumBelow(n - 1);
+  }
+  return 0;
 };
 
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
+  let result = [];
+
+  const recurse = (x, y) => {
+    if (x < y) {
+      result.push(x);
+      recurse(x + 1, y);
+    }
+  };
+
+  recurse(x + 1, y);
+
+  return result;
 };
 
 // 7. Compute the exponent of a number.
@@ -51,6 +86,10 @@ var range = function(x, y) {
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
+  while (exp > 0) {
+    return base * exponent(base, exp - 1)
+  }
+  return 1;
 };
 
 // 8. Determine if a number is a power of two.
